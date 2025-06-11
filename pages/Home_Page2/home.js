de7780b682cf244a1b7c0474257c193da4173ae7
antonioloofy <antonio.junior18@escola.pr.gov.br>
2025-06-11 07:44:09 -0300

    tamanho-de-image-provisorio

---
+++ pages/Home_Page2/home.js
@@ -0,0 +1,12 @@
+
+    function openMenu() {
+        document.getElementById("menu").style.left = "0";
+        document.getElementById("overlay").style.display = "block";
+    }
+
+    function closeMenu() {
+        document.getElementById("menu").style.left = "-250px";
+        document.getElementById("overlay").style.display = "none";
+    }
+
+    
