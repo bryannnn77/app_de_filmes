a00a6453e1207d9cb86ad392d11ce000eb7c05a0
antunes <163547062+antness-s@users.noreply.github.com>
2025-08-18 07:27:20 -0300

    Delete 3Giovane directory

--- 3Giovane/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('suporteBtn').addEventListener('click', function() {
-    const subject = encodeURIComponent("Solicitação de Suporte");
-    const body = encodeURIComponent("Olá, preciso de ajuda com...");
-    window.location.href = `mailto:giovane.campanha@escola.pr.gov.br?subject=${subject}&body=${body}`;
-});
